d226ec3f4d3f30b5341b7d7a6218c5cae98c5696
dmitry.brusentsov <dmitry.brusentsov@sharp-dev.net>
2024-01-25 20:13:35 +0800

    first commit

---
+++ ApiMiniProject/Models/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiMiniProject.Models
+{
+    public class PersonModel
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public bool isActive { get; set; }
+    }
+}
